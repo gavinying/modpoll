3a5e0e47d7c4fa0dd108d56fff0af22f12a30125
ysd <helloysd@foxmail.com>
2021-03-24 00:08:23 +0800

    add test for ysdlog

--- tests/test_ysdtools.py
+++ tests/test_ysdtools.py
@@ -1,5 +1,11 @@
-from ysdtools import __version__
+import ysdtools
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert ysdtools.__version__ == '0.1.0'
+
+
+def test_ysdlog():
+    logger = ysdtools.ysdlog.get_logger("test_ysdtools")
+    logger.debug("logging from test_ysdtools")
+    assert True
