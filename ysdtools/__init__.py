66bd8481f86ed0bff16c0a19f8b2a7e0f90271bb
Ying Shaodong <helloysd@foxmail.com>
2021-09-17 21:10:30 +0800

    add examples

--- ysdtools/__init__.py
+++
@@ -1,9 +0,0 @@
-"""A collection of useful tools
-
-This module includes some personal collection of useful tools.
-It is not designed for production usage. Use it at your own risk.
-"""
-from . import ysdlog
-
-__version__ = "0.1.1"
-__all__ = ["ysdlog"]
