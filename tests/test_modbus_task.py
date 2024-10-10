ee8ebc1db7c2fd29526550e7288f8109dc291e6d
Gavin Ying <helloysd@gmail.com>
2024-10-10 16:21:10 +0800

    chore: Add more tests

---
+++ tests/test_modbus_task.py
@@ -0,0 +1,12 @@
+import threading
+from modpoll.arg_parser import get_parser
+from modpoll.modbus_task import ModbusHandler
+
+
+def test_modbus_task_modbus_setup():
+    parser = get_parser()
+    args = parser.parse_args(["--config", "examples/modsim.csv"])
+    event_exit = threading.Event()
+    for config_file in args.config:
+        modbus_handler = ModbusHandler(args, event_exit)
+        assert modbus_handler.load_config(config_file)
