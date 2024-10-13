36a6fd43e81512af819cd7e00cdef4385e56e7ca
Gavin Ying <helloysd@gmail.com>
2024-10-13 21:43:57 +0800

    feat: Remove importlib-metadata dependency since we drop support for pre-python3.8 environments.

--- modpoll/__init__.py
+++ modpoll/__init__.py
@@ -1,5 +1,3 @@
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
+import importlib.metadata as importlib_metadata
+
 __version__ = importlib_metadata.version(__name__)
